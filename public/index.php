643615a3a19796ca1a6673f2bf8b940c1958c2b3
John Alvaro <jhon54plex@hotmail.com>
2019-07-25 14:00:43 -0500

    Instalando cors

--- public/index.php
+++ public/index.php
@@ -9,11 +9,7 @@
 
 define('LARAVEL_START', microtime(true));
 
-    header("Access-Control-Allow-Origin: *"); 
-    header("Access-Control-Allow-Credentials: true");
-    header('Access-Control-Allow-Methods: GET');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+   
 
 /*
 |--------------------------------------------------------------------------
