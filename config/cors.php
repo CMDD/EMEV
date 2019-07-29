d475a24f482e469cbd261ec01d850736e1a17250
John Alvaro <jhon54plex@hotmail.com>
2019-07-29 10:06:33 -0500

    Agregando actualizarcion de datos

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
-    'allowedHeaders' => ['*'],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
     'allowedMethods' => ['GET','POST'],
     'exposedHeaders' => [],
     'maxAge' => 0,
